1dcef482f311194646ea036006bf24360a0d257a
%berezovskiy_aleksey% <64012423+alex2012001@users.noreply.github.com>
2021-11-19 21:04:32 +0700

    fix animation and effects

--- Assets/ZombieHunter/Weapon/WeaponConfig.cs
+++ Assets/ZombieHunter/Weapon/WeaponConfig.cs
@@ -5,8 +5,6 @@ namespace Weapon
     [CreateAssetMenu(fileName = "WeaponConfig", menuName = "WeaponConfig")]
     public class WeaponConfig : ScriptableObject
     {
-        public Weapon Prefab;
-        public Vector3 SpawnPosition;
         public float Damage;
         public int FireRate;
         public AudioClip ShotSFx;
