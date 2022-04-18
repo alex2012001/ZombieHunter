c74f63297c216808af4662515ab7396d00aa79ad
BerezovskiiAleksey <berezovskialex2001002@gmail.com>
2022-04-18 10:45:29 +0700

    fix demo scene and add startup

---
+++ Assets/ZombieHunter/DemoLevel/DemoEcsStartup.cs
@@ -0,0 +1,45 @@
+﻿using ZombieHunter.EnemyWaveSystem;
+using ZombieHunter.MovementSystem;
+using ZombieHunter.TakeDamageSystem;
+using ZombieHunter.WeaponSystem;
+
+namespace ZombieHunter.DemoLevel
+{
+    public class DemoEcsStartup : EcsStartup
+    {
+        private readonly MovementSystemsContainer _movementSystemsContainer = new MovementSystemsContainer();
+        private readonly WaveSystemContainer _waveSystemContainer = new WaveSystemContainer();
+        private readonly WeaponSystemContainer _weaponSystemContainer = new WeaponSystemContainer();
+        private readonly TakeDamageSystemsContainer _takeDamageSystemsContainer = new TakeDamageSystemsContainer();
+            
+        protected override void AddSystems()
+        {
+            base.AddSystems();
+                
+            _movementSystemsContainer.AddSystems(_systems);
+            //_waveSystemContainer.AddSystems(_systems);
+            _weaponSystemContainer.AddSystems(_systems);
+            //_takeDamageSystemsContainer.AddSystems(_systems);
+        }
+
+        protected override void AddOneFrames()
+        {
+            base.AddOneFrames();
+                
+            _movementSystemsContainer.AddOneFrameObjects(_systems);
+           // _waveSystemContainer.AddOneFrameObjects(_systems);
+            _weaponSystemContainer.AddOneFrameObjects(_systems);
+           // _takeDamageSystemsContainer.AddOneFrameObjects(_systems);
+        }
+
+        protected override void AddInjections()
+        {
+            base.AddInjections();
+                
+            _movementSystemsContainer.AddInjectors(_systems); 
+           // _waveSystemContainer.AddInjectors(_systems);
+            _weaponSystemContainer.AddInjectors(_systems);
+           // _takeDamageSystemsContainer.AddInjectors(_systems);
+        }
+    }
+}
